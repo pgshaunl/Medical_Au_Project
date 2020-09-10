daf26ebb955df6e0ae865849599c133252974a4e
Peigeng Li <2711456@gmail.com>
2020-09-11 01:56:53 +1000

    SearchBox updated

---
+++ MA/app/components/SearchHomeScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SearchBar } from 'react-native-elements';
+
+
+  
+
+
+class Search extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+
+  render() {
+    const { search } = this.state;
+
+    return (
+        <SearchBar
+        platform={"android"}
+        placeholder="Search Hospital..."
+        onChangeText={this.updateSearch}
+        value={search}
+        containerStyle={{width:350, borderRadius:10,
+          margin:10}}
+      />
+      
+      
+    );
+  }
+}
+export default Search;
